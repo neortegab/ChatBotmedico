68de662d817769d7d116f6340808f7f7ab4dc168
Tania240395 <julieth1478@hotmail.com>
2024-10-23 13:04:12 -0500

    docstrings.py
    Documentacion del codigo

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+#Utiliza la biblioteca pandas para leer el archivo 'enfermedades.json' y crea instancias de la clase Enfermedad para cada entrada en el archivo.
 import pandas as pd
 from enfermedad import Enfermedad
 
@@ -10,5 +11,6 @@ def cargar_enfermedades():
     for index, row in df['enfermedades'].items():
         enfermedad = Enfermedad(row['nombre'], row['tipo'], row['sintomas'])
         enfermedades.append(enfermedad)
-    
+    #Returns:
+        #list: Lista de instancias de la clase Enfermedad.
     return enfermedades
