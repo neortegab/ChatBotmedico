7fa868418fd1e3a0cf1fa0642377a964934f712c
Tania240395 <julieth1478@hotmail.com>
2024-10-23 12:45:00 -0500

    docstrings.py
    Documentacion de codigo

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,5 +1,6 @@
 from chat_bot import ChatBot
-
+#Punto de entrada principal del programa.
 if __name__ == "__main__":
+    #Crea una instancia del ChatBot y comienza la interacción con el usuario llamando al método `iniciar_chat()`.
     chatbot = ChatBot()
     chatbot.iniciar_chat()
