0c1f9a9922a9047482989121430836514e03e9a2
Tania240395 <julieth1478@hotmail.com>
2024-10-23 12:48:26 -0500

    docstrings.py
    Documemtacion del codigo

--- src/enfermedad.py
+++ src/enfermedad.py
@@ -1,5 +1,11 @@
+# Clase que representa una enfermedad.
 class Enfermedad:
+    #Inicializa una instancia de la clase Enfermedad.
     def __init__(self, nombre, tipo, sintomas):
+        #Atributos:
+        #nombre (str): El nombre de la enfermedad.
+        #tipo (str): El tipo de enfermedad (por ejemplo, viral, bacteriana, etc.).
+        #sintomas (list): Lista de síntomas asociados con la enfermedad.
         self.nombre = nombre
         self.tipo = tipo
         self.sintomas = sintomas
